336a6629f2d4e333e4cbdbd121c0121f4c35c051
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2022-04-10 11:51:19 +0530

    breadcrumbs set up and office bearers added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", {
+        title: "IEEE LINK - Local Integrated Network of Kerala",
+    });
+});
+
+router.get("/office-bearers/:year", function (req, res, next) {
+  let pagename = "Office Bearers";
+  let breadcrumbs = {
+      head: `Office Bearers ${req.params.year}`,
+      links: [
+          {
+              bc_page: "Office Beearers",
+          },
+          {
+              bc_page: `${req.params.year}`,
+          },
+      ],
+  };
+  res.render(`office-bearers/${req.params.year}`, {
+      title: `${pagename} | IEEE LINK`,
+      breadcrumbs,
+  });
+});
+
+router.get("/contact", function (req, res, next) {
+    let pagename = "Contact";
+    let breadcrumbs = {
+        head: "Contact",
+        links: [
+            {
+                bc_page: "Contact",
+            },
+        ],
+    };
+    res.render("pages/contact", {
+        title: `${pagename} | IEEE LINK`,
+        breadcrumbs,
+    });
 });
 
 module.exports = router;
