d17c04b20fbbb5fdc755b5c4843aa0a16d7c2978
Arjun S <arjun8107@gmail.com>
2023-07-09 15:14:03 +0530

    feat(fe): using js object to add events + added thub 2023 events

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var functionHelper = require("../helpers/functionsHelper");
+const eventList = require("../helpers/eventList");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -82,6 +83,7 @@ router.get("/events/:hub", function (req, res, next) {
    
     res.render(`events/${hub}/${hub}`, {
         title: `${pagename} | IEEE LINK`,
+        eventList: eventList,
         breadcrumbs,
     });
 });
