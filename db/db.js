285aba5eee4e33c8ba18264d5791b7dfb1496524
ABHINAV UNNIKRISHNAN <abhinav200218@gmail.com>
2023-08-02 17:31:08 +0530

    set

---
+++ db/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+const mongo_uri = process.env.MONGO;
+
+mongoose.set(`strictQuery`, false)
+const connect = () => {
+    // console.log(mongo_uri);
+    mongoose
+        .connect(`${mongo_uri}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("successfully connected to database");
+        })
+        .catch((err) => {
+            console.log("Connection failed......");
+            console.log(err);
+        });
+
+};
+
+module.exports = connect;
