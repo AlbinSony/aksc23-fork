c057ec959135697a969b96ae146c7c6e9d187d3b
ABHINAV UNNIKRISHNAN <abhinav200218@gmail.com>
2023-08-03 12:52:04 +0530

    auth

--- db/db.js
+++ db/db.js
@@ -1,25 +1,25 @@
-// const mongoose = require('mongoose')
-// require('dotenv').config();
-// const mongo_uri = process.env.MONGO;
+const mongoose = require('mongoose')
+require('dotenv').config();
+const mongo_uri = process.env.MONGO;
 
-// mongoose.set(`strictQuery`, false)
-// const connect = () => {
-//     // console.log(mongo_uri);
-//     mongoose
-//         .connect(`${mongo_uri}`, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         })
-//         .then(() => {
-//             console.log("successfully connected to database");
-//         })
-//         .catch((err) => {
-//             console.log("Connection failed......");
-//             console.log(err);
-//         });
+mongoose.set(`strictQuery`, false)
+const connect = () => {
+    // console.log(mongo_uri);
+    mongoose
+        .connect(`${mongo_uri}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("successfully connected to database");
+        })
+        .catch((err) => {
+            console.log("Connection failed......");
+            console.log(err);
+        });
 
-// };
+};
 
 
 
-// module.exports = connect;
+module.exports = connect;
